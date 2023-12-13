b55becb247a5e62a7b07f8bf780d2c1008c2f259
Sean Lee <freshdried@gmail.com>
2023-12-13 07:48:44 -0500

    Get widget working.

---
+++ intero/src-tauri/src/widget.rs
@@ -0,0 +1,54 @@
+// Panel
+use crate::panel_ext::{init_as_panel, PanelExt};
+use crate::main_window::position_window;
+use tauri::{AppHandle, Manager, Wry};
+use cocoa::foundation::NSPoint;
+use tauri_nspanel::ManagerExt;
+
+#[tauri::command]
+pub fn show_widget_window(app: AppHandle) {
+    open_widget_window(&app);
+}
+
+pub fn open_widget_window(app: &AppHandle) {
+    if let Some(window) = app.get_window("widget") {
+        window.show().ok();
+        window.set_focus().ok();
+        return;
+    }
+
+    let widget_window = tauri::WindowBuilder::new(
+        app,
+        "widget",
+        tauri::WindowUrl::App("widget.html".into()),
+    )
+    .title("Emojinie widget")
+    .resizable(false)
+    .decorations(false)
+    .transparent(true)
+    .always_on_top(true)
+    .visible(false)
+    .position(0.0, 0.0)
+    .inner_size(600.0, 100.0)
+    .build()
+    .ok();
+
+    if let Some(window) = widget_window {
+        // let _ = window.set_ignore_cursor_events(true);
+        // let _ = window.show().ok();
+        position_window(&window, |display_pos, display_size, win_frame_size| {
+            NSPoint {
+                x: (display_pos.x + (display_size.width)) - (win_frame_size.width),
+                y: (display_pos.y), // 160 from the top
+            }
+        });
+        init_as_panel(window);
+        open_panel(app);
+    }
+}
+
+fn open_panel(handle: &AppHandle<Wry>) {
+  let panel = handle.get_panel("widget").unwrap();
+  panel.show();
+  panel.set_key_window_able2(false);
+}
---
+++ intero/src/widget.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { WidgetView } from "./WidgetView";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <WidgetView />
+);
