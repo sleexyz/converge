713c971ca8759dc0469c39e2c4b424ef8bbaa337
Sean Lee <freshdried@gmail.com>
2024-01-02 12:11:21 -0500

    Format activity picker to show in center.

--- intero/src/App.tsx
+++ intero/src/App.tsx
@@ -2,14 +2,16 @@ import { ActivityPicker } from "./ActivityPicker";
 import "./App.css";
 import { useGlobalShortcut } from "./use_global_shortcut";
 
-
 function App() {
   useGlobalShortcut();
 
-  return <>
-    <ActivityPicker />
-  </>
-
+  return (
+    <div className="fixed bg-black bg-opacity-50 h-full w-full overflow-hidden overscroll-none flex justify-center items-end">
+      <div className="h-[400px] overflow-y-scroll overscroll-none bg-black bg-opacity-90 rounded-xl w-max-[80%] flex flex-col-reverse">
+        <ActivityPicker />
+      </div>
+    </div>
+  );
 }
 
 export default App;
