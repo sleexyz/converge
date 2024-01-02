1ec8a1489863e2781aac376260a46d47d3115fd2
Sean Lee <freshdried@gmail.com>
2024-01-02 12:11:21 -0500

    Format activity picker to show in center.

--- intero/src/ActivityPicker.tsx
+++ intero/src/ActivityPicker.tsx
@@ -1,10 +1,14 @@
 import { useState, useEffect, useRef } from "react";
-import { format } from 'date-fns';
-import { v4 as uuidv4 } from 'uuid';
-import { intervalToDuration, formatDuration } from 'date-fns';
-import { Activity, getActiveActivity, loadState, saveState } from "./state";
-
-const activityStateKey = 'activityState';
+import { format } from "date-fns";
+import { v4 as uuidv4 } from "uuid";
+import { intervalToDuration, formatDuration } from "date-fns";
+import {
+  Activity,
+  activityStateKey,
+  getActiveActivity,
+  loadState,
+  saveState,
+} from "./state";
 
 export function ActivityPicker() {
   const [state, setState] = useState(() => {
@@ -13,90 +17,133 @@ export function ActivityPicker() {
 
   useEffect(() => {
     saveState(activityStateKey, state);
-  }, [state])
-
+  }, [state]);
 
   const [debug, setDebug] = useState(false);
 
-  const [activeActivityId, activeActivity] = getActiveActivity(state)
+  const [activeActivityId, activeActivity] = getActiveActivity(state);
 
   const [showCreateActivity, setShowCreateActivity] = useState(false);
   console.log(showCreateActivity);
 
   return (
     <>
-      <div id="container" className="p-10 flex flex-col justify-start text-left rounded-xl">
-        {!showCreateActivity && activeActivity && !activeActivity.stop && <button className="mt-16 px-4 py-2 text-2xl rounded-md shadow-sm opacity-50 focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-indigo-500" onClick={() => {
-          endCurrentActivity();
-        }}>Stop</button>}
-        {!showCreateActivity && (!activeActivity || (activeActivity && activeActivity.stop)) && <button className="mt-16 px-4 py-2 text-2xl rounded-md shadow-sm opacity-50 focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-indigo-500" onClick={() => {
-          setShowCreateActivity(true);
-        }}>New activity</button>}
+      <div
+        id="container"
+        className="flex flex-col-reverse justify-start text-left"
+      >
+        {!showCreateActivity &&
+          (!activeActivity || (activeActivity && activeActivity.stop)) && (
+            <button
+              className="mt-16 px-4 py-2 text-2xl rounded-md shadow-sm opacity-50 focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              onClick={() => {
+                setShowCreateActivity(true);
+              }}
+            >
+              New activity
+            </button>
+          )}
 
-        <div id="anchor"></div>
+        {showCreateActivity &&
+          (() => {
+            const id = uuidv4();
+            return (
+              <>
+                <ActivityView
+                  id={id}
+                  editable={true}
+                  activity={{}}
+                  newActivity={true}
+                  setActivity={(activity: Activity) => {
+                    endCurrentActivity();
+                    setState((state) => {
+                      return {
+                        ...state,
+                        [id]: activity,
+                      };
+                    });
+                    setShowCreateActivity(false);
+                  }}
+                  deleteActivity={() => {
+                    setShowCreateActivity(false);
+                  }}
+                />
+              </>
+            );
+          })()}
 
-        {showCreateActivity && (() => {
-          const id = uuidv4();
-          return <>
-            <ActivityView
-              className="my-40"
-              id={id} activity={{}} newActivity={true} setActivity={(activity: Activity) => {
-                endCurrentActivity();
-                setState((state) => {
-                  return {
-                    ...state,
-                    [id]: activity,
-                  };
-                });
-                setShowCreateActivity(false);
-              }} deleteActivity={() => {
-                setShowCreateActivity(false);
-              }} />
-          </>
-        })()
-        }
-
-        {Object.entries(state).reverse().map(([id, activity]) => {
-          return <ActivityView
-            className={id === activeActivityId ? "my-36" : ""}
-            key={id} id={id} activity={activity} newActivity={false} setActivity={
-              (activity: Activity) => {
-                setState((state) => {
-                  return {
-                    ...state,
-                    [id]: activity,
-                  };
-                });
-              }
+        {Object.entries(state)
+          .reverse()
+          .map(([id, activity]) => {
+            const isActive = activeActivityId === id;
+            let activityView = (
+              <ActivityView
+                key={id}
+                id={id}
+                editable={isActive}
+                activity={activity}
+                newActivity={false}
+                setActivity={(activity: Activity) => {
+                  setState((state) => {
+                    return {
+                      ...state,
+                      [id]: activity,
+                    };
+                  });
+                }}
+                deleteActivity={() => {
+                  setState((state) => {
+                    const newState = { ...state };
+                    delete newState[id];
+                    return newState;
+                  });
+                }}
+              />
+            );
+            if (isActive) {
+              return (
+                <div
+                  key={id}
+                  className="flex flex-col justify-between flex-grow flex-shrink-0 h-[400px]"
+                >
+                  {activityView}
+                  <button
+                    className="m-8 px-4 py-2 text-2xl rounded-md shadow-sm opacity-50 focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    onClick={() => {
+                      endCurrentActivity();
+                    }}
+                  >
+                    Stop
+                  </button>
+                </div>
+              );
+            } else {
+              return activityView;
             }
-            deleteActivity={() => {
-              setState((state) => {
-                const newState = { ...state };
-                delete newState[id];
-                return newState;
-              });
-            }}
-          />;
-        })}
-
+          })}
       </div>
 
-      {debug && <div className="fixed max-h-screen right-0 p-8 top-0 bg-slate-500 bg-opacity-80 rounded-md overflow-auto">
-        <pre>
-          {JSON.stringify(state, null, 2)}
-        </pre>
-        <button onClick={
-          () => {
-            window.location.reload();
-          }
-        }>
-          reload
-        </button>
-      </div>}
+      {debug && (
+        <div className="fixed max-h-screen right-0 p-8 top-0 bg-slate-500 bg-opacity-80 rounded-md overflow-auto">
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+          <button
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            reload
+          </button>
+        </div>
+      )}
 
-      <button className="opacity-25 fixed right-0 bottom-0" onClick={() => {
-        setDebug(!debug);
-      }}>Toggle debug</button>
+      <button
+        className="opacity-25 fixed right-0 bottom-0"
+        onClick={() => {
+          setDebug(!debug);
+        }}
+      >
+        Toggle debug
+      </button>
     </>
   );
   function endCurrentActivity() {
@@ -109,15 +156,29 @@ export function ActivityPicker() {
       return newState;
     });
   }
-
 }
 
-function ActivityView({ className, newActivity, activity, setActivity, deleteActivity }: { newActivity: boolean, activity: Activity, id: string, setActivity: (activity: Activity) => void, className?: string, deleteActivity: () => void }) {
+function ActivityView({
+  className,
+  newActivity,
+  activity,
+  editable,
+  setActivity,
+  deleteActivity,
+}: {
+  newActivity: boolean;
+  activity: Activity;
+  id: string;
+  setActivity: (activity: Activity) => void;
+  editable?: boolean;
+  className?: string;
+  deleteActivity: () => void;
+}) {
   const [start, setStart] = useState<Date | undefined>(activity.start);
-  const [value, setValue] = useState(activity.value)
+  const [value, setValue] = useState(activity.value);
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key !== 'Enter') {
+    if (e.key !== "Enter") {
       return;
     }
     if (activity.stop) {
@@ -129,7 +190,7 @@ function ActivityView({ className, newActivity, activity, setActivity, deleteAct
       return;
     }
     setStart(new Date());
-  }
+  };
 
   const initialRender = useRef(true);
 
@@ -138,8 +199,8 @@ function ActivityView({ className, newActivity, activity, setActivity, deleteAct
       initialRender.current = false;
       return;
     }
-    save()
-  }, [start])
+    save();
+  }, [start]);
 
   function save() {
     setActivity({
@@ -149,29 +210,30 @@ function ActivityView({ className, newActivity, activity, setActivity, deleteAct
     });
   }
 
-
   const [_interval, setInterval] = useState<number | null>(null);
   const [_tick, setTick] = useState(0);
 
   // TODO: clear interval after timer ends.
   useEffect(() => {
-      setInterval(window.setInterval(() => {
-        setTick(tick => tick + 1);
-      }, 1000));
+    setInterval(
+      window.setInterval(() => {
+        setTick((tick) => tick + 1);
+      }, 1000)
+    );
 
-      return () => {
-        setInterval((interval: number | null) => {
-          if (interval) {
-            window.clearInterval(interval);
-          }
-          return null
-        })
-      }
+    return () => {
+      setInterval((interval: number | null) => {
+        if (interval) {
+          window.clearInterval(interval);
+        }
+        return null;
+      });
+    };
   }, []);
 
   // Seconds remaining:
-  const timeSpentMillis = start ? ((Date.now() - start.getTime())) : 0;
-  const formattedStartDate = start ? format(start, 'h:mm a') : '';
+  const timeSpentMillis = start ? Date.now() - start.getTime() : 0;
+  const formattedStartDate = start ? format(start, "h:mm a") : "";
 
   const ref = useRef<HTMLDivElement>(null);
 
@@ -181,25 +243,30 @@ function ActivityView({ className, newActivity, activity, setActivity, deleteAct
     }
   }, [newActivity]);
 
-
   const duration = intervalToDuration({ start: 0, end: timeSpentMillis });
   const timeSpentString = formatDuration(duration);
 
   return (
-    <div ref={ref} className={["flex flex-col", className].join(" ")}>
+    <div
+      ref={ref}
+      className={["flex flex-col mx-8 mt-12", className].join(" ")}
+    >
       <div className="flex justify-between stretch">
-        <p className="mt-8">{formattedStartDate}</p>
-        <button onClick={deleteActivity} >x</button>
+        <p className="">{formattedStartDate}</p>
+        <button onClick={deleteActivity}>x</button>
       </div>
-      <input
-        type="text"
-        placeholder="What are you committing to single-tasking on right now?"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onKeyDown={handleKeyDown}
-        onBlur={save}
-        className="mt-3 px-4 py-2 text-2xl border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 flex-1"
-      />
+      {editable && (
+        <input
+          type="text"
+          placeholder="What are you committing to single-tasking on right now?"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          onBlur={save}
+          className="mt-3 px-4 py-2 text-2xl border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 flex-1"
+        />
+      )}
+      {!editable && <p className="">{value}</p>}
       {!activity.stop && <p className="mt-8">{timeSpentString}</p>}
     </div>
   );
--- intero/src/WidgetView.tsx
+++ intero/src/WidgetView.tsx
@@ -3,7 +3,7 @@ import { activityStateKey, getActiveActivity, loadState } from "./state";
 import "./WidgetView.css";
 // import { XEyes } from "./XEyes";
 import { listen } from "@tauri-apps/api/event";
-import { intervalToDuration, formatDuration} from 'date-fns';
+import { intervalToDuration, formatDuration } from "date-fns";
 
 interface MouseMoved {
   x: number;
@@ -21,11 +21,15 @@ export function WidgetView() {
 
   const [inWindow, setInWindow] = useState(false);
   useEffect(() => {
-    const unlistenPromise = listen('mouse-moved', (event) => {
+    const unlistenPromise = listen("mouse-moved", (event) => {
       const mouseMoved = event.payload as MouseMoved;
       const x = mouseMoved.x - mouseMoved.window_x;
       const y = mouseMoved.window_height - (mouseMoved.y - mouseMoved.window_y);
-      const cursorInWindow = x >= 0 && x <= mouseMoved.window_width && y >= 0 && y <= mouseMoved.window_height;
+      const cursorInWindow =
+        x >= 0 &&
+        x <= mouseMoved.window_width &&
+        y >= 0 &&
+        y <= mouseMoved.window_height;
       if (cursorInWindow) {
         setInWindow(true);
       } else {
@@ -49,38 +53,39 @@ export function WidgetView() {
     };
   }, []);
 
-
   const [_interval, setInterval] = useState<number | null>(null);
   const [_tick, setTick] = useState(0);
 
   // TODO: clear interval after timer ends.
   useEffect(() => {
-    setInterval(window.setInterval(() => {
-      setTick(tick => tick + 1);
-    }, 1000));
+    setInterval(
+      window.setInterval(() => {
+        setTick((tick) => tick + 1);
+      }, 1000)
+    );
 
     return () => {
       setInterval((interval: number | null) => {
         if (interval) {
           window.clearInterval(interval);
         }
-        return null
+        return null;
       });
     };
   }, []);
 
   const [id, activity] = getActiveActivity(state);
   if (!id) {
-    return <></>
+    return <></>;
   }
 
   // const timeSpentMillis = start ? ((Date.now() - start.getTime())) : 0;
-  const duration = intervalToDuration({ start: activity.start ?? 0, end: new Date() });
+  const duration = intervalToDuration({
+    start: activity.start ?? 0,
+    end: new Date(),
+  });
   const timeSpentString = formatDuration(duration, {
-    format: [
-      "hours",
-      "minutes",
-    ]
+    format: ["hours", "minutes"],
   });
 
   let classes = "transition-opacity duration-150 ease-in-out";
@@ -92,14 +97,14 @@ export function WidgetView() {
 
   return (
     // <XEyes />
-    <div className={["h-screen flex flex-col items-end justify-end", classes].join(" ")}>
+    <div
+      className={["h-screen flex flex-col items-end justify-end", classes].join(
+        " "
+      )}
+    >
       <div className="rounded-xl bg-black p-2 flex flex-col justify-center items-center">
-      <div className="rounded-xl bg-black p-2 text-4xl">
-        {activity.value}
-      </div>
-      <div className="rounded-xl bg-black p-2">
-        {timeSpentString}
-      </div>
+        <div className="rounded-xl bg-black p-2 text-4xl">{activity.value}</div>
+        <div className="rounded-xl bg-black p-2">{timeSpentString}</div>
       </div>
     </div>
   );
