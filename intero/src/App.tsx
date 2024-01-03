8534671283611eea4e7faf33cdf0c9ca35c306aa
Sean Lee <freshdried@gmail.com>
2024-01-03 15:26:04 -0500

    wip

--- intero/src/App.tsx
+++ intero/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useMemo, useCallback } from "react";
 import { ActivityPicker } from "./ActivityPicker";
 import "./App.css";
 import { useGlobalShortcut } from "./use_global_shortcut";
-import { v4 as uuidv4 } from "uuid";
 import { ErrorBoundary } from "./ErrorBoundary";
-import { produce, original, Draft } from "immer";
+import { produce, Draft } from "immer";
+import { Canvas } from "./Canvas";
+import {
+  StateManagerContext,
+  ToposorterState,
+  ToposorterStateData,
+  useLocalStorageState,
+  withNormalization,
+} from "./ToposorterState";
+import * as React from "react";
 
 function App() {
   useGlobalShortcut();
@@ -13,7 +21,7 @@ function App() {
     <>
       <div className="fixed bg-black bg-opacity-50 h-full w-full overflow-hidden overscroll-none flex justify-start items-start">
         <ErrorBoundary>
-          <Toposorter />
+          <ToposorterView />
         </ErrorBoundary>
         <div className="absolute bottom-0 right-0 h-[300px] overflow-y-scroll overscroll-none bg-black bg-opacity-90 rounded-xl flex flex-col-reverse">
           <ErrorBoundary>
@@ -25,107 +33,54 @@ function App() {
   );
 }
 
-type Id = string;
-
-interface Node {
-  value: string;
-  status?: "active" | "done";
-  children: Id[];
-}
-
-interface ToposorterState {
-  nodes: Record<Id, Node>;
-}
-
-function useLocalStorageState<T>(
-  key: string,
-  defaultValue: T,
-  // run migrations here
-  normalizeState: (value: T) => T = (value) => value
-): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [state, setState] = useState<T>(() => {
-    const value = localStorage.getItem(key);
-    if (value) {
-      return normalizeState(JSON.parse(value));
-    }
-    return defaultValue;
-  });
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-
-  return [state, setState];
-}
-
-// Returns topologically sorted nodes.
-function toposort(state: ToposorterState): Id[] {
-  console.log(state);
-  return new Toposort(state.nodes).sort();
-}
-
-class Toposort {
-  visited = new Set<Id>();
-
-  constructor(private readonly nodes: Record<Id, Node>) {}
-
-  // Visit all nodes.
-  // Visit all of a node's children recursively before visiting a node.
-  // Then insertion order should be topologically sorted.
-  sort(): Id[] {
-    for (let key of Object.keys(this.nodes)) {
-      this.visitChildren(key);
-    }
-    return [...this.visited];
-  }
-
-  // DFS
-  private visitChildren(key: Id) {
-    const visted = this.visited.has(key);
-    if (visted) {
-      return;
-    }
-
-    const node = this.nodes[key];
-    if (!node) {
-      return;
-    }
-    for (let childId of node.children) {
-      this.visitChildren(childId);
-    }
-
-    this.visited.add(key);
-  }
-}
-
-function Toposorter() {
-  const [state, setState] = useLocalStorageState<ToposorterState>(
+function ToposorterView() {
+  const [state, setState] = useLocalStorageState<ToposorterStateData>(
     "toposorter",
     {
       nodes: {},
     },
-    produce((_draft: Draft<ToposorterState>) => {})
+    produce((_draft: Draft<ToposorterStateData>) => {})
   );
 
   const [input, setInput] = useState("");
   const [error, setError] = useState<null | Error>(null);
 
-  function trySetState(fn: (value: ToposorterState) => ToposorterState) {
+  const trySetState = useCallback((
+    fn: (value: ToposorterStateData) => ToposorterStateData
+  ) => {
     setState((state) => {
       try {
         return fn(state);
       } catch (e: unknown) {
+        console.error(e);
         setError(e as Error);
         return state;
       }
     });
-  }
+  }, [setState, setError]);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
+
+  const stateManager = useMemo(() => {
+    return {
+      addNode: (value: string) => {
+        trySetState(withNormalization(ToposorterState.addNode(value)));
+      },
+      deleteNode: (idPrefix: string) => {
+        trySetState(withNormalization(ToposorterState.deleteNode(idPrefix)));
+      },
+      addEdge: (fromPrefix: string, toPrefix: string) => {
+        trySetState(withNormalization(ToposorterState.addEdge(fromPrefix, toPrefix)));
+      },
+      setStatus: (idPrefix: string, status: string) => {
+        trySetState(withNormalization(ToposorterState.setStatus(idPrefix, status)));
+      },
+    };
+  }, [setState, trySetState]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key !== "Enter") {
@@ -137,22 +92,30 @@ function Toposorter() {
       if (input.startsWith("/delete")) {
         const args = input.split(" ");
         let id = args[1];
-        deleteNode(id);
-        normalize();
+        stateManager.deleteNode(id);
       } else if (input.startsWith("/child")) {
         const args = input.split(" ");
         let from = args[1];
         let to = args[2];
-        addEdge(from, to);
-        normalize();
+        stateManager.addEdge(from, to);
       } else if (input.startsWith("/status")) {
         const args = input.split(" ");
         let idPrefix = args[1];
         let status = args[2];
-        setStatus(idPrefix, status);
+        stateManager.setStatus(idPrefix, status);
+      } else if (input.startsWith("/done")) {
+        const args = input.split(" ");
+        let idPrefix = args[1];
+        stateManager.setStatus(idPrefix, 'done');
+      } else if (input.startsWith("/active")) {
+        const args = input.split(" ");
+        let idPrefix = args[1];
+        stateManager.setStatus(idPrefix, 'active');
+      } else if (input.startsWith("/")) {
+        const args = input.split(" ");
+        throw new Error(`Unknown command ${args[0]}`);
       } else {
-        addNode(input);
-        normalize();
+        stateManager.addNode(input);
       }
       setError(null);
       setInput("");
@@ -162,96 +125,6 @@ function Toposorter() {
     }
   };
 
-  function normalize() {
-    trySetState(
-      produce((draft: Draft<ToposorterState>) => {
-        const state = original(draft)!;
-        draft.nodes = {};
-        for (let id of toposort(state)) {
-          draft.nodes[id] = state.nodes[id];
-        }
-      })
-    );
-  }
-
-  function setStatus(idPrefix: string, status: string) {
-    trySetState(
-      produce((draft: Draft<ToposorterState>) => {
-        let nodes = original(draft.nodes)!;
-        let id = Object.keys(nodes).find((id) => id.startsWith(idPrefix));
-        if (!id) {
-          throw new Error(`Could not find node with prefix ${idPrefix}`);
-        }
-        if (status !== "active" && status !== "done") {
-          throw new Error(
-            `Invalid status ${status}. Expected "active" or "done"`
-          );
-        }
-        draft.nodes[id].status = status as "active" | "done";
-      })
-    );
-  }
-
-  function deleteNode(idPrefix: string) {
-    trySetState(
-      produce((draft: Draft<ToposorterState>) => {
-        // reconcile ids
-        let nodes = original(draft.nodes)!;
-        let id = Object.keys(nodes).find((id) => id.startsWith(idPrefix));
-        if (!id) {
-          throw new Error(`Could not find node with prefix ${idPrefix}`);
-        }
-
-        // Delete the node.
-        delete draft.nodes[id];
-
-        // Delete all edges to this node.
-        for (const key of Object.keys(nodes)) {
-          if (key === id) {
-            continue;
-          }
-          const newChildren = nodes[key].children.filter(
-            (childId) => childId !== id
-          );
-          draft.nodes[key].children = newChildren;
-        }
-      })
-    );
-  }
-
-  function addEdge(fromPrefix: string, toPrefix: string) {
-    trySetState(
-      produce((draft: Draft<ToposorterState>) => {
-        // reconcile ids
-        let nodes = original(draft.nodes)!;
-        let from = Object.keys(nodes).find((id) => id.startsWith(fromPrefix));
-        let to = Object.keys(nodes).find((id) => id.startsWith(toPrefix));
-        console.log(from, to);
-        if (!from) {
-          throw new Error(
-            `Could not find child node with prefix ${fromPrefix}`
-          );
-        }
-        if (!to) {
-          throw new Error(`Could not find parent node with prefix ${toPrefix}`);
-        }
-        draft.nodes[from].children.push(to);
-      })
-    );
-  }
-
-  function addNode(value: string) {
-    trySetState(
-      produce((draft: ToposorterState) => {
-        const id = uuidv4();
-        draft.nodes[id] = {
-          value,
-          children: [],
-        };
-      })
-    );
-  }
-
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.currentTarget) {
       setInput(e.currentTarget.value);
@@ -259,12 +132,15 @@ function Toposorter() {
   };
 
   return (
-    <div className="flex flex-col items-start m-20 space-y-8 p-8 w-[50%] rounded-xl h-[80%] bg-black bg-opacity-90">
-      <h1 className="text-4xl text-white">Toposorter</h1>
-      <div className="bg-white bg-opacity-10 flex flex-col-reverse w-full items-end overflow-y-scroll">
-        {error && <pre className="text-red-500">{error.message}</pre>}
-        <pre className="flex-1">{JSON.stringify(state, null, 2)}</pre>
-      </div>
+    <div className="flex flex-col items-start m-[2%] space-y-8 p-8 w-[60%] rounded-xl h-[96%] bg-black">
+      <StateManagerContext.Provider value={stateManager}>
+        <Canvas nodes={state.nodes} />
+      </StateManagerContext.Provider>
+      {error && (
+        <pre className="bg-red-100 text-red-500 m-8 p-8 rounded-xl text-bold absolute right-0 top-0">
+          Error: {error.message}
+        </pre>
+      )}
       <input
         ref={inputRef}
         type="text"
---
+++ intero/src/ToposorterState.tsx
@@ -0,0 +1,235 @@
+import { Draft, original, produce } from "immer";
+import * as React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export type Id = string;
+
+export interface TNode {
+  value: string;
+  createdAt: Date;
+  status?: "active" | "done";
+  children: Id[];
+}
+
+export function useLocalStorageState<T>(
+  key: string,
+  defaultValue: T,
+  // run migrations here
+  normalizeState: (value: T) => T = (value) => value
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = React.useState<T>(() => {
+    const value = localStorage.getItem(key);
+    if (value) {
+      return normalizeState(parseJSON(value));
+    }
+    return defaultValue;
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
+
+function parseJSON<T>(json: string): T {
+  try {
+    return JSON.parse(json, (key, value) => {
+      if (key === "createdAt") {
+        return new Date(value);
+      }
+      return value;
+    });
+  } catch (e) {
+    throw new Error(`Could not parse JSON: ${e}`);
+  }
+}
+
+function statusToPoints(status: "active" | "done" | undefined): number {
+  switch (status) {
+    case "done":
+      return 2;
+    case "active":
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+export function withNormalization(
+  fn: (state: ToposorterStateData) => ToposorterStateData = (state) => state
+) {
+  return (state: ToposorterStateData) => {
+    return produce((draft: Draft<ToposorterStateData>) => {
+      const state = original(draft)!;
+      draft.nodes = {};
+      const entries = Object.entries(state.nodes).sort(
+        ([_keyA, a], [_keyB, b]) => {
+          const statusDiff = statusToPoints(b.status) - statusToPoints(a.status);
+          if (statusDiff !== 0) {
+            return statusDiff;
+          }
+          // prefer later createdAt
+          const timeDiff = b.createdAt.getTime() - a.createdAt.getTime();
+          if (timeDiff !== 0) {
+            return timeDiff;
+          }
+          return Math.random() - 0.5;
+        }
+      );
+      for (let id of Toposort.sort(entries)) {
+        (draft.nodes[id] as any) = {};
+        Object.assign(draft.nodes[id], state.nodes[id]);
+        if (state.nodes[id].createdAt == undefined) {
+          draft.nodes[id].createdAt = new Date();
+        }
+      }
+    })(fn(state));
+  };
+}
+
+export class Toposort {
+  visited = new Set<Id>();
+  nodes: Record<Id, TNode> = {};
+
+  private constructor(entries: [key: Id, value: TNode][]) {
+    this.nodes = Object.fromEntries(entries);
+  }
+
+  // Returns topologically sorted nodes.
+  static sort(entries: [key: Id, value: TNode][]): Id[] {
+    return new Toposort(entries).sort();
+  }
+
+  // Visit all nodes.
+  // Visit all of a node's children recursively before visiting a node.
+  // Then insertion order should be topologically sorted.
+  sort(): Id[] {
+    for (let key of Object.keys(this.nodes)) {
+      this.visitChildren(key);
+    }
+    return [...this.visited];
+  }
+
+  // DFS
+  private visitChildren(key: Id) {
+    const visted = this.visited.has(key);
+    if (visted) {
+      return;
+    }
+
+    const node = this.nodes[key];
+    if (!node) {
+      return;
+    }
+    for (let childId of node.children) {
+      this.visitChildren(childId);
+    }
+
+    this.visited.add(key);
+  }
+}
+
+export interface ToposorterStateData {
+  nodes: Record<Id, TNode>;
+}
+
+export const StateManagerContext = React.createContext<{
+  addNode: (value: string) => void;
+  deleteNode: (idPrefix: string) => void;
+  addEdge: (fromPrefix: string, toPrefix: string) => void;
+  setStatus: (idPrefix: string, status: string) => void;
+} | null>(null);
+
+export class ToposorterState {
+  constructor(readonly state: ToposorterStateData) {}
+
+  getNodes(): Record<Id, TNode> {
+    return this.state.nodes;
+  }
+
+  reconcileId(idPrefix: string): Id | undefined {
+    let ret = undefined;
+    for (let id of Object.keys(this.state.nodes)) {
+      if (id.startsWith(idPrefix)) {
+        if (ret) {
+          throw new Error(`Multiple ids match prefix ${idPrefix}`);
+        }
+        ret = id;
+      }
+    }
+    return ret;
+  }
+
+  static setStatus(idPrefix: string, status: string) {
+    return produce((draft: Draft<ToposorterStateData>) => {
+      let nodes = original(draft.nodes)!;
+      let id = Object.keys(nodes).find((id) => id.startsWith(idPrefix));
+      if (!id) {
+        throw new Error(`Could not find node with prefix ${idPrefix}`);
+      }
+      if (status === "unset") {
+        delete draft.nodes[id].status;
+        return;
+      }
+      if (status !== "active" && status !== "done") {
+        throw new Error(
+          `Invalid status ${status}. Expected "active" or "done" or "unset"`
+        );
+      }
+      draft.nodes[id].status = status as "active" | "done";
+    });
+  }
+
+  static deleteNode(idPrefix: string) {
+    return produce((draft: Draft<ToposorterStateData>) => {
+      const state = new ToposorterState(original(draft)!);
+      let nodes = state.getNodes();
+      let id = state.reconcileId(idPrefix);
+      if (!id) {
+        throw new Error(`Could not find node with prefix ${idPrefix}`);
+      }
+
+      // Delete the node.
+      delete draft.nodes[id];
+
+      // Delete all edges to this node.
+      for (const key of Object.keys(nodes)) {
+        if (key === id) {
+          continue;
+        }
+        const newChildren = nodes[key].children.filter(
+          (childId) => childId !== id
+        );
+        draft.nodes[key].children = newChildren;
+      }
+    });
+  }
+
+  static addEdge(fromPrefix: string, toPrefix: string) {
+    return produce((draft: Draft<ToposorterStateData>) => {
+      const state = new ToposorterState(original(draft)!);
+      let from = state.reconcileId(fromPrefix);
+      let to = state.reconcileId(toPrefix);
+      console.log(from, to);
+      if (!from) {
+        throw new Error(`Could not find child node with prefix ${fromPrefix}`);
+      }
+      if (!to) {
+        throw new Error(`Could not find parent node with prefix ${toPrefix}`);
+      }
+      draft.nodes[from].children.push(to);
+    });
+  }
+
+  static addNode(value: string) {
+    return produce((draft: ToposorterStateData) => {
+      const id = uuidv4();
+      draft.nodes[id] = {
+        value,
+        createdAt: new Date(),
+        children: [],
+      };
+    });
+  }
+}
