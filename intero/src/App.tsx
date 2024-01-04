74a57248e62b6696ca9f36a919a7d33592966295
Sean Lee <freshdried@gmail.com>
2024-01-04 14:29:37 -0500

    Bind command args based on selection.

--- intero/src/App.tsx
+++ intero/src/App.tsx
@@ -10,99 +10,55 @@ import {
   StateManagerContext,
   ToposorterState,
   ToposorterStateData,
+  ToposorterStateProvider,
+  useError,
   useLocalStorageState,
+  useToposorterState,
   withNormalization,
 } from "./ToposorterState";
 import { ReactFlowProvider } from "reactflow";
 import { SelectionPane } from "./SelectionPane";
+import { SelectionProvider } from "./Selection";
 
 function App() {
   useGlobalShortcut();
 
   return (
     <ErrorBoundary>
-      <ToposorterView />
+      <ReactFlowProvider>
+        <ToposorterStateProvider>
+          <SelectionProvider>
+            <ToposorterView />
+          </SelectionProvider>
+        </ToposorterStateProvider>
+      </ReactFlowProvider>
     </ErrorBoundary>
   );
 }
 
 function ToposorterView() {
-  const [state, setState] = useLocalStorageState<ToposorterStateData>(
-    "toposorter",
-    {
-      nodes: {},
-    },
-    produce((_draft: Draft<ToposorterStateData>) => {})
-  );
-
-  const [error, setError] = useState<null | Error>(null);
-
-  const trySetState = useCallback(
-    (fn: (value: ToposorterStateData) => ToposorterStateData) => {
-      setState((state) => {
-        try {
-          return fn(state);
-        } catch (e: unknown) {
-          console.error(e);
-          setError(e as Error);
-          return state;
-        }
-      });
-    },
-    [setState, setError]
-  );
-
-  const stateManager = useMemo(() => {
-    return {
-      addNode: (value: string) => {
-        trySetState(withNormalization(ToposorterState.addNode(value)));
-      },
-      deleteNode: (idPrefix: string) => {
-        trySetState(withNormalization(ToposorterState.deleteNode(idPrefix)));
-      },
-      addEdge: (fromPrefix: string, toPrefix: string) => {
-        trySetState(
-          withNormalization(ToposorterState.addEdge(fromPrefix, toPrefix))
-        );
-      },
-      setStatus: (idPrefix: string, status: string) => {
-        trySetState(
-          withNormalization(ToposorterState.setStatus(idPrefix, status))
-        );
-      },
-      setValue: (idPrefix: string, value: string) => {
-        trySetState(
-          withNormalization(ToposorterState.setValue(idPrefix, value))
-        );
-      },
-    };
-  }, [setState, trySetState]);
+  const state = useToposorterState();
+  const error = useError();
 
   return (
-    <ReactFlowProvider>
-      <StateManagerContext.Provider value={stateManager}>
-        <SetErrorContext.Provider value={setError}>
-          <div className="absolute bg-black bg-opacity-50 h-full w-full flex justify-start items-start">
-            <div className="flex-1 flex flex-col items-start m-[3%] space-y-8 p-8 w-[94%] rounded-xl h-[94%] bg-black">
-              <Canvas nodes={state.nodes} />
-              {error && (
-                <pre className="bg-red-100 text-red-500 m-8 p-8 rounded-xl text-bold absolute right-0 top-0">
-                  Error: {error.message}
-                </pre>
-              )}
-            </div>
-            <div className="flex-0 flex-grow-0 flex-shrink flex flex-col items-start m-[3%] w-[600px] space-y-8 rounded-xl h-[94%]">
-              <div className="flex-0 basis-[300px] flex flex-col-reverse w-full overscroll-none overflow-y-scroll bg-black rounded-xl">
-                <ErrorBoundary>
-                  <ActivityPicker />
-                </ErrorBoundary>
-              </div>
-              <SelectionPane />
-            </div>
-          </div>
-        </SetErrorContext.Provider>
-      </StateManagerContext.Provider>
-    </ReactFlowProvider>
+    <div className="absolute bg-black bg-opacity-50 h-full w-full flex justify-start items-start">
+      <div className="flex-1 flex flex-col items-start m-[3%] space-y-8 p-8 w-[94%] rounded-xl h-[94%] bg-black">
+        <Canvas nodes={state.getNodes()} />
+      </div>
+      <div className="flex-0 flex-grow-0 flex-shrink flex flex-col items-start m-[3%] w-[600px] space-y-8 rounded-xl h-[94%]">
+        {error && (
+          <pre className="bg-red-100 text-red-500 m-8 p-8 rounded-xl text-bold fixed right-0 top-0">
+            Error: {error.message}
+          </pre>
+        )}
+        <div className="flex-0 basis-[300px] flex flex-col-reverse w-full overscroll-none overflow-y-scroll bg-black rounded-xl">
+          <ErrorBoundary>
+            <ActivityPicker />
+          </ErrorBoundary>
+        </div>
+        <SelectionPane />
+      </div>
+    </div>
   );
 }
 
--- intero/src/SelectionPane.tsx
+++ intero/src/SelectionPane.tsx
@@ -1,44 +1,20 @@
-import { useContext, useEffect, useMemo, useRef, useState } from "react";
-import { Edge, Node, useOnSelectionChange } from "reactflow";
-import { Id, StateManagerContext, TNode } from "./ToposorterState";
+import { useContext, useEffect, useState } from "react";
+import { Id, StateManagerContext, TNode, ToposorterStateContext } from "./ToposorterState";
 import ReactTextareaAutosize from "react-textarea-autosize";
 import { format } from "date-fns";
 import { CommandLine } from "./CommandLine";
-
-// Only update the value when it is not null
-function useLastValue<T>(value: T | null): T | null {
-  const ref = useRef<T | null>(null);
-  if (value) {
-    ref.current = value;
-  }
-  return ref.current!;
-}
+import { useSelectedNode } from "./Selection";
 
 export function SelectionPane() {
-  const [selectedNodes, setSelectedNodes] = useState<Node[]>([]);
-  const [selectedEdges, setSelectedEdges] = useState<Edge[]>([]);
-  useOnSelectionChange({
-    onChange: ({ nodes, edges }) => {
-      setSelectedNodes(nodes);
-      setSelectedEdges(edges);
-    },
-  });
-
-  const selectedNode = selectedNodes.length > 0 ? selectedNodes[0] : null;
-
-  const lastSelectedNode = useLastValue(selectedNode);
-  const tnode = lastSelectedNode?.data as TNode | null;
-
-  let borderColor =
-    selectedNode == null ? "border-transparent" : "border-indigo-500";
-  borderColor = "border-transparent";
+  const selectedNode = useSelectedNode();
+  const tnode = selectedNode?.data as TNode | undefined;
 
   return (
     <div
-      className={`flex-1 flex flex-col bg-black w-full rounded-xl p-8 border ${borderColor} justify-between`}
+      className={`flex-1 flex flex-col bg-black w-full rounded-xl p-8 justify-between`}
     >
       <div className="flex flex-col items-stretch">
-        {tnode && <SelectionEditor tnode={tnode} id={lastSelectedNode!.id} />}
+        {tnode && <SelectionEditor tnode={tnode} id={selectedNode!.id} />}
       </div>
       <CommandLine />
     </div>
@@ -46,6 +22,7 @@ export function SelectionPane() {
 }
 
 function SelectionEditor({ tnode, id }: { tnode: TNode; id: Id }) {
+  const state = useContext(ToposorterStateContext)!;
   const stateManager = useContext(StateManagerContext)!;
   const [nodeValue, setNodeValue] = useState<string | null>(null);
 
@@ -63,7 +40,8 @@ function SelectionEditor({ tnode, id }: { tnode: TNode; id: Id }) {
     setNodeValue(event.target.value);
     // Update the node value in the state manager
     // Assuming there's a method updateNode in stateManager
-    stateManager.setValue(id, event.target.value);
+    const node = state.reconcileId(id);
+    stateManager.setValue(node, event.target.value);
   };
 
   // Format as "September 5, 2021 at 12:00 PM"
@@ -78,7 +56,9 @@ function SelectionEditor({ tnode, id }: { tnode: TNode; id: Id }) {
           placeholder="Node value"
           className="text-left box-content text-2xl rounded-md shadow-sm opacity-80 focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-500 border-gray-600 m-0 p-2 border"
         />
-        <span className="text-sm text-gray-500">Created on {formattedDate}</span>
+        <span className="text-sm text-gray-500">
+          Created on {formattedDate}
+        </span>
       </div>
     </>
   );
