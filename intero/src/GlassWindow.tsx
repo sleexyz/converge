a85518e1f1d3f16bc041241cb6092969a4878bca
Sean Lee <freshdried@gmail.com>
2024-02-08 14:56:17 -0800

    Dim screen when no item is active.

--- intero/src/GlassWindow.tsx
+++ intero/src/GlassWindow.tsx
@@ -1,14 +1,17 @@
-export function GlassWindow(props: {
+import { forwardRef } from "react";
+
+export const GlassWindow = forwardRef(function GlassWindow(props: {
   children?: React.ReactNode;
   className: string;
-}) {
+}, ref) {
   return (
     <div
       className={`bg-white bg-opacity-75 p-2 shadow-xl rounded-2xl ${props.className}`}
+      ref={ref}
     >
       <div className="bg-white border-gray-300 border rounded-2xl h-full w-full">
         {props.children}
       </div>
     </div>
   );
-}
+});
